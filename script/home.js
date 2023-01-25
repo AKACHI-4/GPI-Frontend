9c5ea4846830986217e189846d5b954d96e7f770
AKACHI-4 <Adarshrawat8304@gmail.com>
2023-01-25 16:27:26 +0530

    Modifying frontend .. Coding part done

---
+++ script/home.js
@@ -0,0 +1,12 @@
+var getStartedContainer = document.getElementById("getStartedContainer");
+if (getStartedContainer) {
+    getStartedContainer.addEventListener("click", function (e) {
+        window.location.href = "./faculty-form.html";
+    });
+}
+var loginText = document.getElementById("loginText");
+if (loginText) {
+    loginText.addEventListener("click", function (e) {
+        window.location.href = "./faculty-form.html";
+    });
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,42 @@
-const colors = require('tailwindcss/colors')
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./*.{html,js}"],
   theme: {
     extend: {
-      colors: { 
-        brand: { 
-          DEFAULT: "#0D0D2B",
-          icon: "#3671E9",
-          button: "#3671E9",
-          white10: "#FFFFFF1A"
-        }
+      colors: {
+        gray: {
+          "100": "#d9d9d9",
+          "200": "#a1a1a4",
+          "300": "#8996ac",
+          "400": "#6f7d95",
+          "500": "#494a50",
+          "600": "#323b4a",
+          "700": "#0d1117",
+          "800": "#0d1116",
+        },
+        black: "#000",
+        indigo: { "100": "#1c68e4", "200": "#3251be" },
+        turquoise: "#32be83",
+        orange: "#be9732",
+        "colors-neutral-gray-4": "#e4e4e4",
+        "divider-light-1px-60": "rgba(228, 228, 228, 0.6)",
+        "neutral-colors-white": "#fff",
       },
+      fontFamily: {
+        "button-text-bold": "Inter",
+        arial: "Arial",
+        abeezee: "ABeeZee",
+      },
+      borderRadius: { small: "5px", base: "10px", large: "15px" },
+    },
+    fontSize: {
+      xs: "15px",
+      sm: "16px",
+      base: "18px",
+      lg: "20px",
+      xl: "22px",
+      "2xl": "30px",
     },
   },
-  variants: {},
-  plugins: [],
-}
+  corePlugins: { preflight: false },
+};
