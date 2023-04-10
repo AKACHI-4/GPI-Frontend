1dff625bec491b9954bcbc4b4c6f1a878c8f5a3e
AKACHI-4 <Adarshrawat8304@gmail.com>
2023-04-10 21:18:01 +0530

    Form added
    - Responsive Student Form added in GPI/Student-form page

--- src/Components/Form1.js
+++ src/Components/Form1.js
@@ -1,3 +1,4 @@
+import { Subject } from '@mui/icons-material';
 import React, { useState } from 'react';
 
 function Form1() {
@@ -15,25 +16,104 @@ function Form1() {
 
   return (
     <>  
-        <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input
-          type="text"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        />
-      </label>
-      <label>
-        Email:
-        <input
-          type="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+      <form onSubmit={handleSubmit} class="mt-4 max-w-md mx-auto">
+        <div class="mb-4">
+          <label class="block text-gray-700 font-bold mb-2" for="name">
+            Name:
+          </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="name"
+            type="text"
+            // value={name}
+            // onChange={(event) => setName(event.target.value)}
+            placeholder="Enter your name"
+          />
+        </div>
+        <div class="mb-4">
+          <label class="block text-gray-700 font-bold mb-2" for="email">
+            Email:
+          </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            // value={email}
+            // onChange={(event) => setEmail(event.target.value)}
+            placeholder="Enter your email"
+          />
+        </div>
+        <div class="mb-4">
+          <label class="block text-gray-700 font-bold mb-2" for="name">
+            Course:
+          </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="course"
+            type="text"
+            // value={course}
+            // onChange={(event) => setCourse(event.target.value)}
+            placeholder="Enter Course"
+          />
+        </div>
+        <div class="mb-4">
+          <label class="block text-gray-700 font-bold mb-2" for="name">
+            Section:
+          </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="section"
+            type="text"
+            // value={section}
+            // onChange={(event) => setName(event.target.value)}
+            placeholder="Enter Section"
+          />
+        </div>
+        <div class="mb-4">
+          <label class="block text-gray-700 font-bold mb-2" for="name">
+            Subject:
+          </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="subject"
+            type="text"
+            // value={subject}
+            // onChange={(event) => setName(event.target.value)}
+            placeholder="Enter Subject"
+          />
+        </div>
+        <div class="mb-4">
+          <label class="block text-gray-700 font-bold mb-2" for="name">
+            Subject Code:
+          </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="subjectcode"
+            type="text"
+            // value={subjectcode}
+            // onChange={(event) => setName(event.target.value)}
+            placeholder="Enter Subject Code"
+          />
+        </div>
+        <div class="mb-4">
+          <label class="block text-gray-700 font-bold mb-2" for="name">
+            Max Radius(in meter):
+          </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="maxradius"
+            type="text"
+            // value={maxradius}
+            // onChange={(event) => setName(event.target.value)}
+            placeholder="Enter Max Radius"
+          />
+        </div>
+        <div class="mt-6 flex items-center justify-center">
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
     </>
   )
 }
