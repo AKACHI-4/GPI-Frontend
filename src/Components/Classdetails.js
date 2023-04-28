791154a9539b24c69d752559d0a63a26de27ddd3
AKACHI-4 <Adarshrawat8304@gmail.com>
2023-04-28 22:16:39 +0530

    Card added !!

--- src/Components/Classdetails.js
+++ src/Components/Classdetails.js
@@ -3,12 +3,12 @@ import React from 'react'
 function Classdetails() {
   return (
     <div>
-      <ul>
-        <li className="text-white font-bold text-xl">Course : --</li>
-        <li className="text-white font-bold text-xl">Section : --</li>
-        <li className="text-white font-bold text-xl">Subject : --</li>
-        <li className="text-white font-bold text-xl">Subject Code : --</li>
-        <li className="text-white font-bold text-xl">Faculty Name : --</li>
+      <ul className='flex flex-col gap-2'>
+        <li className="text-white font-bold text-xl">Course : </li>
+        <li className="text-white font-bold text-xl">Section : </li>
+        <li className="text-white font-bold text-xl">Subject : </li>
+        <li className="text-white font-bold text-xl">Subject Code : </li>
+        <li className="text-white font-bold text-xl">Faculty Name : </li>
       </ul>
     </div>
   )
