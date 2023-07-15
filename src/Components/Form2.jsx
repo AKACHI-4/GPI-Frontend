a9c072f8f64d83973910b955b1ff64a68c9ec514
AKACHI-4 <Adarshrawat8304@gmail.com>
2023-07-15 22:05:54 +0530

    chore: Prettified

--- src/Components/Form2.jsx
+++ src/Components/Form2.jsx
@@ -1,29 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import yaml from 'js-yaml';
-import { useLocation } from 'react-router-dom';
-import '../index.css'; 
+import React, { useState, useEffect } from "react";
+import yaml from "js-yaml";
+import { useLocation } from "react-router-dom";
+import "../index.css";
 
 function Form2() {
-  const location = useLocation(); 
-  const urls = location.pathname.split('/');
-    
+  const location = useLocation();
+  const urls = location.pathname.split("/");
+
   const [studentData, setData] = useState({
-    name: '',
-    rollno: 0, 
-    studentid: '',
-    latitude: 0.0, 
+    name: "",
+    rollno: 0,
+    studentid: "",
+    latitude: 0.0,
     longitude: 0.0,
-    present: false
-  })
+    present: false,
+  });
   const [submitStatus, setsubmitStatus] = useState(false);
   const [formError, setformError] = useState(false);
 
-  useEffect(() => { 
+  useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
-      setData(prevData => ({
-        ...prevData, 
-        latitude: position.coords.latitude, 
-        longitude: position.coords.longitude
+      setData((prevData) => ({
+        ...prevData,
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
       }));
     });
   }, []);
@@ -31,41 +31,42 @@ function Form2() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if(studentData.name && studentData.rollno && studentData.studentid) {
+    if (studentData.name && studentData.rollno && studentData.studentid) {
       const yamlData = yaml.dump(studentData);
-      fetch(`/student-data/${urls[2]}/${urls[3]}`, { 
-        method : 'POST', 
-        headers : { 
-          'Content-type' : 'application/x-yaml'
-        }, 
+      fetch(`/student-data/${urls[2]}/${urls[3]}`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/x-yaml",
+        },
         body: yamlData,
-      }) 
-      .then((res) => res.json())
-      .then((result) => {
-        console.log(result); 
-        setsubmitStatus(true);
-
-        setTimeout(() => {
-          setsubmitStatus(false);
-        }, 10000)
-
       })
-      .catch((err) => { 
-        console.error(err); 
-      })
-    } else {  
+        .then((res) => res.json())
+        .then((result) => {
+          console.log(result);
+          setsubmitStatus(true);
+
+          setTimeout(() => {
+            setsubmitStatus(false);
+          }, 500);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    } else {
       setformError(true);
 
       setTimeout(() => {
         setformError(false);
-      }, 10000); 
+      }, 500);
     }
-
-  }
+  };
 
   return (
     <>
-      <form onSubmit={handleSubmit} className="border-4 border-gray-700 rounded-2xl py-10 px-6 w-11/12 lg:w-2/6">
+      <form
+        onSubmit={handleSubmit}
+        className="border-4 border-gray-700 rounded-2xl py-10 px-6 w-11/12 lg:w-2/6"
+      >
         {/* Name */}
         <div className="mb-4">
           <label className="block text-gray-700 font-bold mb-2" htmlFor="name">
@@ -76,7 +77,7 @@ function Form2() {
             id="name"
             type="text"
             value={studentData.name}
-            onChange={(e) => setData({...studentData, name: e.target.value})}
+            onChange={(e) => setData({ ...studentData, name: e.target.value })}
             placeholder="Enter your name"
           />
         </div>
@@ -90,33 +91,40 @@ function Form2() {
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline "
             id="rollno"
             type="text"
-            value={studentData.rollno > 0 ? studentData.rollno : ''}
-            onChange={(e) => setData({...studentData, rollno: e.target.value})}
+            value={studentData.rollno > 0 ? studentData.rollno : ""}
+            onChange={(e) =>
+              setData({ ...studentData, rollno: e.target.value })
+            }
             placeholder="Enter Roll no"
             inputMode="numeric"
             style={{ MozAppearance: "textfield", appearance: "textfield" }}
             pattern="[0-9]*"
-          /> 
+          />
         </div>
 
         {/* Student ID */}
         <div className="mb-12">
           <label className="block text-gray-700 font-bold mb-2" htmlFor="name">
-              Student ID:
+            Student ID:
           </label>
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="Studentid"
             type="string"
             value={studentData.studentid}
-            onChange={(e) => setData({...studentData, studentid: e.target.value})}
+            onChange={(e) =>
+              setData({ ...studentData, studentid: e.target.value })
+            }
             placeholder="Enter Student ID"
           />
         </div>
 
         {/* Submit */}
         <div className="flex items-center justify-center">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
             Submit
           </button>
         </div>
@@ -132,10 +140,9 @@ function Form2() {
             Your details are submitted !!
           </div>
         )}
-
       </form>
     </>
-  )
+  );
 }
 
-export default Form2
+export default Form2;
